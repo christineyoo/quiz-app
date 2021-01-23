0f744c68f0d2291324664955a0ea56f8352270ad
Christine Yoo <christinecho.s@gmail.com>
2021-01-22 18:29:19 -0800

    added css and scoring

--- app.js
+++ app.js
@@ -1,97 +1,89 @@
 const store = {
   questions: [
     {
-      question: 'Question 1 of 5: How do you say the Greek symbol &theta; ?',
-      answers: [
-        'alpha',
-        'beta',
-        'gamma',
-        'theta'
-      ],
-      correctAnswer: 'theta'
+      question: "Question 1 of 5: How do you say the Greek symbol &theta; ?",
+      answers: ["alpha", "beta", "gamma", "theta"],
+      correctAnswer: "theta",
     },
     {
-      question: 'Question 2 of 5: Given a right triangle, which ratio represents tan &theta; ?',
+      question: "Question 2 of 5: Which ratio represents tan &theta; ?",
       answers: [
-        'opposite/adjacent',
-        'opposite/hypotenuse',
-        'adjacent/hypotenuse',
-        'adjacent/opposite'
+        "opposite/adjacent",
+        "opposite/hypotenuse",
+        "adjacent/hypotenuse",
+        "adjacent/opposite",
       ],
-      correctAnswer: 'opposite/adjacent'
+      correctAnswer: "opposite/adjacent",
     },
     {
-      question: 'Question 3 of 5: What trigonometric function does the following graph represent?',
+      question: "Question 3 of 5: Which ratio represents sin &theta; ?",
       answers: [
-        'y = tan(x)',
-        'y = csc(x)',
-        'y = sin(x)',
-        'y = cos(x)'
+        "opposite/adjacent",
+        "opposite/hypotenuse",
+        "adjacent/hypotenuse",
+        "adjacent/opposite",
       ],
-      correctAnswer: 'y = sin(x)'
+      correctAnswer: "opposite/hypotenuse",
     },
     {
-      question: 'Question 4 of 5: Convert the following expression from radians to degrees.',
-      answers: [
-        '90 degrees',
-        '180 degrees',
-        '270 degrees',
-        'QT &#928;'
-      ],
-      correctAnswer: '90 degrees'
+      question: "Question 4 of 5: Convert &#928;/2 from radians to degrees.",
+      answers: ["90 degrees", "180 degrees", "270 degrees", "QT &#928;"],
+      correctAnswer: "90 degrees",
     },
     {
-      question: 'Question 5 of 5: Which trigonometric function is an even function?',
+      question:
+        "Question 5 of 5: Which trigonometric function is an even function?",
       answers: [
-        'sin(-x) = - sin(x)',
-        'csc(-x) = - csc(x)',
-        'cos(-x) = cos(x)',
-        'tan(-x) = -tan(x)'
+        "sin(-x) = - sin(x)",
+        "csc(-x) = - csc(x)",
+        "cos(-x) = cos(x)",
+        "tan(-x) = -tan(x)",
       ],
-      correctAnswer: 'cos(-x) = cos(x)'
-    }
-  ]
+      correctAnswer: "cos(-x) = cos(x)",
+    },
+  ],
+  quizStarted: false,
+  questionNumber: 1,
+  score: 0,
 };
 
-/////////////GLOBAL VARIABLES AND CONSTANTS////////////////////
-let quizStarted = false;
-let questionNumber = 1;
-let score = 0;
-const intro = "<h2>How well do you know your trigonometry?</h2><button id='start'>Start Quiz</button>";
-//////////////////////////////////////////////////////////////
+const intro =
+  "<h2>How well do you know your trigonometry?</h2><button id='start'>Start Quiz</button>";
 
 //*** Responsible for generating the question template
 function handleShowProblem() {
   // The question and answers variables change as the questionNumber variable changes
-  let { question } = store.questions[questionNumber - 1];
-  let { answers }  = store.questions[questionNumber - 1];
-  let problem = '';
+  let { question } = store.questions[store.questionNumber - 1];
+  let { answers } = store.questions[store.questionNumber - 1];
+  let problem = "";
   for (let i = 0; i < store.questions.length; i++) {
-      problem = `<form>
+    problem = `<form>
       <h3>${question}</h3>
-      <input type="radio" name="question" value="${answers[0]}" class="radio"> ${answers[0]}<br>
-      <input type="radio" name="question" value="${answers[1]}" class="radio"> ${answers[1]}<br>
-      <input type="radio" name="question"value="${answers[2]}" class="radio"> ${answers[2]}<br>
-      <input type="radio" name="question" value="${answers[3]}" class="radio"> ${answers[3]}<br>
-      <button id="submit">Submit</button>
+      <input type="radio" name="question" value="${answers[0]}" class="radio" id="answer-1"><label for="answer-1"> ${answers[0]}</label><br>
+      <input type="radio" name="question" value="${answers[1]}" class="radio" id="answer-2"><label for="answer-2"> ${answers[1]}</label><br>
+      <input type="radio" name="question"value="${answers[2]}" class="radio" id="answer-3"><label for="answer-3"> ${answers[2]}</label><br>
+      <input type="radio" name="question" value="${answers[3]}" class="radio" id="answer-4"><label for="answer-4"> ${answers[3]}</label><br>
+      <button id="submit" disabled>Submit</button>
       <button id="next">Next Question</button></form>
-      <div id="feedback"></div>`
+      <p>Score: ${store.score} </p>
+      <div id="feedback"></div>
+      `;
   }
   return problem;
 }
 
 function handleShowResults() {
-  return `<h3>Your final score is ${score}.</h3><br>
+  return `<form><h3>Your final score is ${store.score} out of 5.</h3><br>
   <h4>Click "Restart Game" to try again!</h4>
-  <button id="restart">Restart Game</button>`
+  <button id="restart">Restart Game</button></form>`;
 }
 
 //***Responsible for rendering the landing page
 function renderLandingPage() {
   $("main").html(intro);
-  $("#start").on("click", function(e) {
+  $("#start").on("click", function (e) {
     e.preventDefault();
-    quizStarted = true;
+    store.quizStarted = true;
     $("main").html(handleShowProblem());
     $("#next").hide();
     handleSubmit();
@@ -104,49 +96,42 @@ function renderNextQuestion() {
   $("#next").hide();
   handleSubmit();
 }
-  
+
 //***When the user clicks on an answer AND clicks on "Submit" button, the handleFeedback function is fired
 function handleSubmit() {
-  console.log("handleSubmit function ran");
   // if any one of the radio inputs is selected
-  $("input").on("click", function(e){
+  $("input").on("click", function (e) {
     const selectedAnswer = $(this).val();
-    const { correctAnswer } = store.questions[questionNumber - 1];
-    $("#submit").on("click", function(e){
+    const { correctAnswer } = store.questions[store.questionNumber - 1];
+    $("#submit").removeAttr("disabled");
+    $("#submit").on("click", function (e) {
       e.preventDefault();
       $("#submit").hide();
       $("#next").show();
-      if (selectedAnswer === correctAnswer){
-        score++;
+      if (selectedAnswer === correctAnswer) {
+        store.score++;
         $("#feedback").html("<p>Correct!</p>");
-        $("#feedback").append(`<p>Score: ${score} </p>`);
       } else {
-        $("#feedback").html(`<p>Incorrect. The correct answer was ${correctAnswer}.</p>`)
+        $("#feedback").html(
+          `<p>Incorrect. The correct answer was ${correctAnswer}.</p>`
+        );
       }
     });
   });
 
-  // If the user clicks submit without clicking on an answer, then a pop up should appear
-  $("#submit").on("click", function(e) {
-      e.preventDefault();
-      if(!($(".radio").is(':checked'))) {
-          alert("Select one before submitting.");
-      }
-  });
-
-  $("#next").on("click", function(e){
+  $("#next").on("click", function (e) {
     e.preventDefault();
-    if (questionNumber < 5) {
-      questionNumber++;
+    if (store.questionNumber < 5) {
+      store.questionNumber++;
       renderNextQuestion();
     } else {
-    $("main").html(handleShowResults());
-    $("#restart").on("click", function(e){
-      questionNumber = 1;
-      score = 0;
-      renderLandingPage();
-    });
-  }
+      $("main").html(handleShowResults());
+      $("#restart").on("click", function (e) {
+        store.questionNumber = 1;
+        store.score = 0;
+        renderLandingPage();
+      });
+    }
   });
 }
 
